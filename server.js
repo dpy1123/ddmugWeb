030b214ba88e47883b6bf82cd38f2eef1c2504c0
dd <dpy1123@126.com>
2015-09-16 18:33:03 +0800

    server

--- server.js
+++ server.js
@@ -22,18 +22,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/api/shows', function(req, res, next) {
-  var query = Show.find();
+  var query = Music.find();
   /*if (req.query.genre) {
     query.where({ genre: req.query.genre });
   } else if (req.query.alphabet) {
     query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') });
   } else {
     query.limit(12);
-  }
+  }*/
   query.exec(function(err, shows) {
     if (err) return next(err);
     res.send(shows);
-  });*/
+  });
 });
 app.get('/api/shows/:id', function(req, res, next) {
 	var id = req.params.id;
@@ -53,13 +53,71 @@ app.use(function(err, req, res, next) {
 
 
 
-
-
-
-
-
-
-
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+var musicSchema = new mongoose.Schema({
+	_id: Number,
+	name: String,
+	description: String,
+	playtimes: Number,
+	create_date: Date,
+	mp3: [{
+		url: String,
+		uploader: String,
+		validated: Boolean
+	}],
+	maps: [{
+		map: String,
+		type: String,
+		uploader: String,
+		upload_date: Date
+	}]
+});
+var userSchema = new mongoose.Schema({
+	_id: Number,
+	name: String,
+	email: { type: String, unique: true },
+  password: String,
+	description: String,
+	create_date: Date
+});
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+var recordSchema = new mongoose.Schema({
+	_id: Number,
+	music_id: Number,
+	user_id: Number,
+	type: String,
+	difficulty: String,
+	score: Number,
+	description: String,
+	create_date: Date
+});
+var User = mongoose.model('User', userSchema);
+var Music = mongoose.model('Music', musicSchema);
+
+mongoose.connect('localhost');
+
+
+
